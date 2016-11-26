31f94afec508930c8d2dc6c32d91416fca5c9dff
root <root@gamma.mh00.net>
2016-11-26 15:14:44 +0300

    Added configs, PHAR scripts

---
+++ config/mod_shop.php
@@ -0,0 +1,23 @@
+<?php
+//config('shop_strade_secret')
+return [
+	'shop' => true,
+	
+	'max_daily' => true,
+	'max_daily_sum' => 20, //per each level
+	
+	'items_per_trade' => 100, 
+	
+	'games_need' => true,
+	'games_need_count' => 1,
+
+	'steam_price_%' => 100,
+	'dep_comission_from' => 50,
+	'dep_comission_%' => 5,
+	
+	'garbadge_from' => 10,
+	'garbadge_%' => 30,
+	
+	'shop_strade_port' => '80',
+	'shop_strade_ip' => '46.105.42.220'
+];
---
+++ config/pay.php
@@ -0,0 +1,6 @@
+<?php
+return [
+  'freekassa_id' => env('PAY_ID'),
+  'freekassa_s1' => env('PAY_SEC1'),
+  'freekassa_s2' => env('PAY_SEC2')
+];
