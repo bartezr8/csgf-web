5af6cc4f916bce4e617863b50ffd2344a00248dc
SKONIKS <skolya16@ya.ru>
2017-01-05 20:40:42 +0300

    Also fix

--- app/Http/Controllers/GiftsController.php
+++ app/Http/Controllers/GiftsController.php
@@ -18,33 +18,36 @@ class GiftsController extends Controller {
     }
     public function checkWinners(Request $request)
     {
-        $ids = $request->get('users'); $users = [];
-        foreach($ids as $id){
-            $user = User::find($id);
-            if(in_array($user,$users)) continue;
-            $lastBet = $user->lastBet();
-            if(is_null($lastBet)) continue;
-            if((Carbon::parse($lastBet->created_at)->timestamp + 600) < Carbon::now()->timestamp) continue;
-            $lastGift = DB::table('gifts')->where('user_id', $user)->first();
-            if(!is_null($lastGift)) continue;
-            $users[] = $user;
-        }
-        if(count($users) > 0){
-            $giftsdb = DB::table('gifts')->where('game_type', '<', 3)->where('sold', 0)->get();
-            if(count($giftsdb) > 0){
-                $gifts = []; foreach($giftsdb as $gift) $gifts[] = $gift;
-                $gift = $gifts[rand(0, (count($gifts) - 1))];
-                $user = $user[rand(0, (count($user) - 1))];
-                DB::table('gifts')->where('id', $gift->id)->update(['user_id' => $user->id, 'sold' => 1, 'sold_at' => Carbon::now()->toDateTimeString()]);
-                $value = [
-                    'steamid' => $user->steamid64,
-                    'game_name' => $gift->game_name,
-                    'store_price' => $gift->store_price,
-                    'user_ava' => $user->avatar
-                ];
-                $this->redis->publish('gifts', json_encode($value));
+        if(config('mod_game.gifts')){
+            $ids = $request->get('users'); $users = [];
+            foreach($ids as $id){
+                $user = User::find($id);
+                if(in_array($user,$users)) continue;
+                $lastBet = $user->lastBet();
+                if(is_null($lastBet)) continue;
+                if((Carbon::parse($lastBet->created_at)->timestamp + 600) < Carbon::now()->timestamp) continue;
+                $lastGift = DB::table('gifts')->where('user_id', $user)->first();
+                if(!is_null($lastGift)) continue;
+                $users[] = $user;
+            }
+            if(count($users) > 0){
+                $giftsdb = DB::table('gifts')->where('game_type', '<', 3)->where('sold', 0)->get();
+                if(count($giftsdb) > 0){
+                    $gifts = []; foreach($giftsdb as $gift) $gifts[] = $gift;
+                    $gift = $gifts[rand(0, (count($gifts) - 1))];
+                    $user = $user[rand(0, (count($user) - 1))];
+                    DB::table('gifts')->where('id', $gift->id)->update(['user_id' => $user->id, 'sold' => 1, 'sold_at' => Carbon::now()->toDateTimeString()]);
+                    $value = [
+                        'steamid' => $user->steamid64,
+                        'game_name' => $gift->game_name,
+                        'store_price' => $gift->store_price,
+                        'user_ava' => $user->avatar
+                    ];
+                    $this->redis->publish('gifts', json_encode($value));
+                }
             }
         }
+        return response()->json(['sucsses']);
     }
     public function receiveGift()
     {
@@ -57,17 +60,19 @@ class GiftsController extends Controller {
     }
     public function selectGiftWinner(Request $request)
     {
-        $user = User::find($request->get('user'));
-        $gift = DB::table('gifts')->where('id', $request->get('id'))->first();
-        if(is_null($user) || is_null($gift)) return redirect('/gifts/admin');
-        DB::table('gifts')->where('id', $gift->id)->update(['user_id' => $user->id, 'sold' => 1, 'sold_at' => Carbon::now()->toDateTimeString()]);
-        $value = [
-            'steamid' => $user->steamid64,
-            'game_name' => $gift->game_name,
-            'store_price' => $gift->store_price,
-            'user_ava' => $user->avatar
-        ];
-        $this->redis->publish('gifts', json_encode($value));
+        if(config('mod_game.gifts')){
+            $user = User::find($request->get('user'));
+            $gift = DB::table('gifts')->where('id', $request->get('id'))->first();
+            if(is_null($user) || is_null($gift)) return redirect('/gifts/admin');
+            DB::table('gifts')->where('id', $gift->id)->update(['user_id' => $user->id, 'sold' => 1, 'sold_at' => Carbon::now()->toDateTimeString()]);
+            $value = [
+                'steamid' => $user->steamid64,
+                'game_name' => $gift->game_name,
+                'store_price' => $gift->store_price,
+                'user_ava' => $user->avatar
+            ];
+            $this->redis->publish('gifts', json_encode($value));
+        }
         return redirect('/gifts/admin');
     }
 }
