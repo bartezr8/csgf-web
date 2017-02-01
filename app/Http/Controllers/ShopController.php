cba8c8493900170181c1fb0be7485d0d4a5e3693
SKONIKS <skolya16@ya.ru>
2017-02-01 17:30:44 +0300

    Поменял ссылку в магазине.

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -443,7 +443,7 @@ class ShopController extends Controller {
             'accessToken' => $this->user->accessToken
         ];
         $shop_id = Shop::selectBot();
-        $out = GameController::curl( env('APP_URL') . '/sendTrade/'.$shop_id.'/?data='.json_encode($value).'&secretKey=' . config('app.secretKey'));
+        $out = GameController::curl( config('app.url') . '/sendTrade/'.$shop_id.'/?data='.json_encode($value).'&secretKey=' . config('app.secretKey'));
         $out = json_decode($out, true);
         if(isset($out['success']) && $out['success'] == true) {
             $id = DB::table('shop_offers')->insertGetId([
