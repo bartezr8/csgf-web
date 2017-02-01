3d913e311b1e1b3758bd794db59892001e1950a3
SKONIKS <skolya16@ya.ru>
2017-02-01 22:21:46 +0300

    Обновление админки. Перенесены все запросы с Forms[get] на ajax[post] (без перезагрузки страницы) Убраны лишние запросы, переписан контроллер. Убраны команды у модераторов. Обновленная цензура, работающая через BD.

---
+++ resources/views/includes/admin_head.php
@@ -0,0 +1,14 @@
+<div class="admin-container">
+    <div class="admin-top">
+        <div class="logotype active">
+        </div>
+        <div class="admin-menu">
+            <ul id="headNav" class="list-reset">
+                <li class="faq"><a href="/admin/"><img src="/assets/img/stav.png" alt=""> Главная страница</a></li>
+                <li class="faq"><a href="/admin/users/"><img src="/assets/img/user.png" alt=""> Пользователи</a></li>
+                <li class="faq"><a href="/admin/cens/"><img src="/assets/img/tp.png" alt=""> Цензура</a></li>
+                <li class="faq"><a href="/shop/admin/"><img src="/assets/img/php.png" alt=""> История обменов</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
