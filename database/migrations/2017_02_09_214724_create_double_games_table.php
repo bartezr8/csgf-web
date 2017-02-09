d8554f651ece6a592d06aaf0f692bbe4d38072af
skoniks <w3csgf@beta.mh00.net>
2017-02-09 21:47:38 +0300

    Migration update

---
+++ database/migrations/2017_02_09_214724_create_double_games_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateDoubleGamesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('double_games', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('num')->unsigned()->nullable();
+			$table->integer('status')->unsigned();
+			$table->bigInteger('price');
+			$table->integer('am');
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('double_games');
+	}
+
+}
---
+++ database/migrations/2017_02_09_214724_create_gdonate_payments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateGdonatePaymentsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('gdonate_payments', function(Blueprint $table)
+		{
+			$table->integer('id', true);
+			$table->string('gdonateId');
+			$table->string('account');
+			$table->float('sum', 10, 0);
+			$table->integer('itemsCount')->default(1);
+			$table->timestamp('dateCreate')->default(DB::raw('CURRENT_TIMESTAMP'));
+			$table->dateTime('dateComplete')->nullable();
+			$table->boolean('status')->default(0);
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('gdonate_payments');
+	}
+
+}
